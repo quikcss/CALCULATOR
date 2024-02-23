5c6e46b5f4b4bff8ad157f37728a9bdcffc4bac5
quikcss <139520657+quikcss@users.noreply.github.com>
2024-02-23 22:38:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+let calcout = document.querySelector(".output")
+
+let calscreen = document.querySelector(".calculator-output")
+
+let buttons = document.querySelectorAll(".input")
+
+let string = ""
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string)
+            string = string.toString()
+            if(string.length>15){
+                document.getElementById("h1").style.fontSize = "20px" 
+            }
+            calcout.textContent = string
+        }
+        else if(e.target.innerHTML == 'C'){
+            string = string.substring(0, string.length - 1);
+            calcout.textContent = string
+        }
+        else if (e.target.innerHTML == "AC") {
+            string = ""
+            calcout.textContent = string
+            document.getElementById("h1").style.fontSize = "4vw"
+        }
+        else if (string.length >= 15) {
+            window.alert("Cant enter more than 15 numbers")
+        }
+        else {
+            string += e.target.innerHTML
+            calcout.textContent = string
+            if (string.length >= 7) {
+                document.getElementById("h1").style.fontSize = "40px"
+            }
+        }
+    })
+})
